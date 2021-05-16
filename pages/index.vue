917332e90b628ef32d228d33c2b4fba54cd58d77
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2021-05-17 00:30:53 +0200

    Remove bounce effect

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
       <p class="text-center text-3xl">
         Coming Soon!
       </p>
-      <div class="flex justify-center animate-bounce">
+      <div class="flex justify-center">
         <img
           src="../static/pexels-rodnae-productions-6936081.jpg"
           class="m-8 p-8 object-cover "
