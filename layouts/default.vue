d04a2ac92c8ade126716d212f7b0f3256e950fd0
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2021-05-18 19:10:42 +0200

    Updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,12 @@
           </template>
           <template #right>
             <vs-navbar-item :active="active == 'license'" id="license">
-              Join List
+              <a
+                href="https://form.jotform.com/211375284893060"
+                target="_blank"
+              >
+                Join List</a
+              >
             </vs-navbar-item>
           </template>
         </vs-navbar>
@@ -64,10 +69,9 @@
           </template>
 
           <vs-sidebar-item id="donate">
-            <template #icon>
-              😁
-            </template>
-            Join List
+            <a href="https://form.jotform.com/211375284893060" target="_blank">
+              😁Join List</a
+            >
           </vs-sidebar-item>
         </vs-sidebar>
       </div>
@@ -141,23 +145,6 @@
                 <path
                   d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"
                 ></path>
-              </svg>
-            </a>
-            <a class="ml-3 text-gray-500">
-              <svg
-                fill="currentColor"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="0"
-                class="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="none"
-                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                ></path>
-                <circle cx="4" cy="4" r="2" stroke="none"></circle>
               </svg>
             </a>
           </span>
--- pages/index.vue
+++ pages/index.vue
@@ -16,23 +16,36 @@
             </p>
           </template>
           <template #interactions>
-            <vs-button success>Whatsapp </vs-button>
-            <vs-button primary>Twitter</vs-button>
-            <vs-button dark>Phone</vs-button>
-            <vs-button style="background-color:#519657;"
-              ><svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
-                ></path>
-                <path
-                  d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
-                ></path></svg
-            ></vs-button>
+            <a
+              class="text-muted"
+              href="https://api.whatsapp.com/send?phone=263777150345"
+            >
+              <vs-button success>Whatsapp </vs-button></a
+            >
+
+            <a class="text-dark" href="tel:+263777150345"
+              ><vs-button dark>Phone</vs-button></a
+            >
+
+            <a class="text-dark" href="https://www.twitter.com/allwegotismind">
+              <vs-button primary>Twitter</vs-button></a
+            >
+            <a class="text-dark" href="https://allwegotismind.netlify.app">
+              <vs-button style="background-color:#519657;"
+                ><svg
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+                  ></path>
+                  <path
+                    d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+                  ></path></svg
+              ></vs-button>
+            </a>
           </template>
         </vs-card>
       </div>
@@ -51,23 +64,40 @@
             </p>
           </template>
           <template #interactions>
-            <vs-button success>Whatsapp </vs-button>
-            <vs-button primary>Twitter</vs-button>
-            <vs-button dark>Phone</vs-button>
-            <vs-button style="background-color:#519657;"
-              ><svg
-                class="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
-                ></path>
-                <path
-                  d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
-                ></path></svg
-            ></vs-button>
+            <a
+              class="text-muted"
+              href="https://api.whatsapp.com/send?phone=263777150345"
+            >
+              <vs-button success>Whatsapp </vs-button></a
+            >
+
+            <a class="text-dark" href="tel:+263777150345"
+              ><vs-button dark>Phone</vs-button></a
+            >
+
+            <a class="text-dark" href="https://www.twitter.com/allwegotismind">
+              <vs-button primary>Twitter</vs-button></a
+            >
+            <a
+              class="text-dark"
+              href="https://allwegotismind.netlify.app"
+              target="_blank"
+            >
+              <vs-button style="background-color:#519657;"
+                ><svg
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+                  ></path>
+                  <path
+                    d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+                  ></path></svg
+              ></vs-button>
+            </a>
           </template>
         </vs-card>
       </div>
