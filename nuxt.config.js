ec97d314c07510246905a69afc144162f2fc00be
Cyprian Tinashe Aarons <cyprianaarons263@gmail.com>
2021-05-18 18:36:08 +0200

    updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,13 +4,13 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "allwegotismind",
+    title: "AllWeGotIsMind | A place to get connected to the help you need.",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
